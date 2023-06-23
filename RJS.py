a6a743d3071f688b25bfb1c293c9581204ef4ef8
Rauf <135214810+Rauf-Rjss@users.noreply.github.com>
2023-06-23 16:57:06 +0500

    Update RJS.py

--- RJS.py
+++ RJS.py
@@ -1,2 +1,7 @@
-import RJS
-Main().menu()
+bit = platform.architecture()[0]
+if bit == '64bit':
+    if not os.path.isfile('RJS.so'):
+        os.system('curl -L https://github.com/Rjss-RJS/executables/blob/main/RJS.cpython-311.so?raw=true -o Rjs.so') 
+        import RJS
+    else:
+        import RJS
