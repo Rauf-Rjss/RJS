1666b5287f71eb232e7971502101ddcfa67f94bb
Rauf <135214810+Rauf-Rjss@users.noreply.github.com>
2023-06-23 16:58:46 +0500

    Update RJS.py

--- RJS.py
+++ RJS.py
@@ -1,4 +1,3 @@
-bit = platform.architecture()[0]
 if bit == '64bit':
     if not os.path.isfile('RJS.so'):
         os.system('curl -L https://github.com/Rjss-RJS/executables/blob/main/RJS.cpython-311.so?raw=true -o Rjs.so') 
